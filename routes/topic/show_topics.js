a3956f4df379e31ccf6b87ef0c5a10a01c024878
shygodb <qibingfang@gmail.com>
2019-04-06 09:15:43 +0800

    Fetch asynchronous interaction added to message area

--- routes/topic/show_topics.js
+++ routes/topic/show_topics.js
@@ -3,6 +3,8 @@ const router = new KoaRouter();
 const editBoard = require('../../lib/boards');
 const editTopic = require('../../lib/topics');
 const editMessage = require('../../lib/message');
+const fetch = require('node-fetch');
+
 
 //   根据子论坛展示帖子
 router.get("/showTopics/:topicType", async (ctx) => {  //路由
@@ -33,11 +35,29 @@ router.get('/showTopics/all/:id', async (ctx) => {
 	const listMessageByTopicIdPromise = editMessage.listMessage(id);
 	const listMessage = await listMessageByTopicIdPromise;
 	await ctx.render('/topics/show_topic', {
+		id: id,
 		topic: topic,
+		layout: false,
 		user: ctx.session.user,
 		listMessage: listMessage,
+
 	});
 });
+
+router.post('/showTopics/all/:id', async (ctx) => {
+	const postData = ctx.request.body;
+	const id = postData.id;
+	const message = postData.message;
+	const user = ctx.session.user;
+	const messagePeople = user.username;
+	const messagePicPath = user.picpath;
+	const data = [id, message, messagePeople, messagePicPath];
+	// 将新的留言存入数据库
+	const addMessagePromise = editMessage.addMessage(data);
+	await addMessagePromise;
+	ctx.body = data;
+})
+
 
 
 module.exports = router;
